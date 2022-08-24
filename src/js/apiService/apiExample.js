6e93e17cb06f9aa6b57df4eeacb321dae6a3bd90
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-24 07:34:37 +0300

    addedApiMethods

--- src/js/apiService/apiExample.js
+++ src/js/apiService/apiExample.js
@@ -4,11 +4,17 @@ const refs = {
     popularBtn: document.querySelector("#popular_request"),
     searchBtn: document.querySelector("#search_request"), 
     movieByIdBtn: document.querySelector("#movie_id_request"), 
+    genresBtn: document.querySelector("#genres_request"), 
+    trailersBtn: document.querySelector("#trailers_request"), 
 }
 
 refs.popularBtn.addEventListener('click', consolePopular)
 refs.searchBtn.addEventListener('click', consoleSearch)
 refs.movieByIdBtn.addEventListener('click', consoleMovieById)
+refs.genresBtn.addEventListener('click', consoleGenres)
+refs.trailersBtn.addEventListener('click', consoleTrailers)
+
+
 
 async function consolePopular() {
     const data = await API.getPopularMovies(1)
@@ -22,5 +28,15 @@ async function consoleSearch() {
 
 async function consoleMovieById() {
     const data = await API.getMovieById(756999)
+    console.log(data)
+}
+
+async function consoleGenres() {
+    const data = await API.getGenres()
+    console.log(data)
+}
+
+async function consoleTrailers() {
+    const data = await API.getMovieTrailers(756999)
     console.log(data)
 }
--- src/js/apiService/movieAPI.js
+++ src/js/apiService/movieAPI.js
@@ -6,33 +6,57 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const POPULAR_URL = `${BASE_URL}/trending/movie/week`;
 const SEARCH_URL = `${BASE_URL}/search/movie`;
 const MOVIE_ID_URL = `${BASE_URL}/movie/`;
+const GENRES_URL = `${BASE_URL}/genre/movie/list`;
 
+const SORT_BY_CREATED_ASC = `created_at.asc`;
+const SORT_BY_CREATED_DESC = `created_at.desc`;
 
 
 export default {
-    async getPopularMovies(page) {
-        try {
-          const response = await axios.get(`${POPULAR_URL}?api_key=${API_KEY}&page=${page}`);
-          return response.data;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    async getSearchMovies(query, page) {
-        try {
-          const data = await axios.get(`${SEARCH_URL}?api_key=${API_KEY}&query=${query}&page=${page}`);
-          return data;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    async getMovieById(id) {
-        try {
-          const data = await axios.get(`${MOVIE_ID_URL}${id}?api_key=${API_KEY}`); 
-          return data;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    
-}
+  async getPopularMovies(page) {
+    try {
+      const response = await axios.get(
+        `${POPULAR_URL}?api_key=${API_KEY}&page=${page}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async getSearchMovies(query, page) {
+    try {
+      const {data} = await axios.get(
+        `${SEARCH_URL}?api_key=${API_KEY}&query=${query}&page=${page}`
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async getMovieById(id) {
+    try {
+      const {data} = await axios.get(`${MOVIE_ID_URL}${id}?api_key=${API_KEY}`);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async getGenres() {
+    try {
+      const {data} = await axios.get(`${GENRES_URL}?api_key=${API_KEY}`);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async getMovieTrailers(id) {
+    try {
+      const {data} = await axios.get(
+        `${MOVIE_ID_URL}${id}/videos?api_key=${API_KEY}`
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
