ed3c305f45ac348c7f23917fad8b98ae66177c12
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-26 00:02:40 +0300

    options

---
+++ src/js/pagination.js
@@ -0,0 +1,14 @@
+import { Pagination } from "tui-pagination";
+
+const options = {
+    totalItems: 0,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+    centerAlign: true,
+}
+
+//const container = document.getElementById('tui-pagination-container');
+//const instance = new Pagination(container, { ... });
+
+//instance.getCurrentPage();
