3bd8db8c95e1d8ca500d9568e7f6426d1a4de0d9
Stanislav2022 <tsv5@ukr.net>
2022-08-25 22:18:05 +0300

    6
    замінив зображення

--- src/js/apiService/createGallery.js
+++ src/js/apiService/createGallery.js
@@ -1,9 +1,5 @@
 import API from './movieAPI';
-const refs = {
-  popularBtn: document.querySelector('#popular_request'),
-};
-
-refs.popularBtn.addEventListener('click', consolePopular);
+consolePopular();
 
 async function consolePopular() {
   const data = await API.getPopularMovies(1);
@@ -17,8 +13,8 @@ function createGallery(filmsArry) {
       (acc += `
             <li class="films__item">
               <a class="films__link" href="">
-                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${
-                  item.backdrop_path
+                <img class="films__img" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${
+                  item.poster_path
                 }" alt="" />
                 <div class="films__decor">
                   <h2 class="films__title">${item.title}</h2>
