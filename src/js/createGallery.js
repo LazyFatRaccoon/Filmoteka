da74865ed6e82b608b3e2d0fc14dd1ac6748f450
Stanislav2022 <tsv5@ukr.net>
2022-08-26 22:46:53 +0300

    1

--- src/js/createGallery.js
+++ src/js/createGallery.js
@@ -16,6 +16,33 @@ async function consoleModifiedPopular() {
   });
 }
 
+function createGanresFilmArray(ganresArray) {
+  if (ganresArray.length !== 0) {
+    let ganresFilm = [];
+    const ganresName = ganresArray.reduce((acc, item) => {
+      ganresFilm.push(` ${item}`);
+    }, 0);
+    if (ganresFilm.length > 2) {
+      ganresFilm.push(' Other');
+      ganresFilm.splice(2, ganresFilm.length - 3);
+    }
+    return ganresFilm;
+  }
+  return `No ganre`;
+}
+function createYearFilm(year) {
+  if (year) {
+    return year;
+  }
+  return `No year`;
+}
+function createPosterFilm(posterLink) {
+  // if (posterLink) {
+  //   return `https://image.tmdb.org/t/p/w600_and_h900_bestv2${posterLink}`;
+  // }
+  return `/src/images/no-poster-available.jpg`;
+}
+
 const filmsListEl = document.querySelector('.films__list');
 
 export function createGallery(filmsArry) {
@@ -25,15 +52,14 @@ export function createGallery(filmsArry) {
       (acc += `
             <li class="films__item" id="${item.id}">
               <div class="films__shell">
-                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${
-                  item.poster_path
-                }" alt="" />
+                <img src="${createPosterFilm(item.poster_path)}" alt="${
+        item.overview
+      }" />
                 <div class="films__decor">
                   <h2 class="films__title">${item.title}</h2>
-                  <p class="films__description">Drama | ${item.release_date.slice(
-                    0,
-                    4
-                  )}</p>
+                  <p class="films__description">${createGanresFilmArray(
+                    item.genre_ids
+                  )} | ${createYearFilm(item.release_date.slice(0, 4))}</p>
                 </div>
              </div>
             </li>`),
