86d44c59557b31fd9523d5c8d000d6c51c00262c
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-08-26 11:09:18 +0300

    added_serchTojs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 import './js/apiService/apiExample'
+=======
+import './js/apiService/apiExample';
+import './js/createGallery';
+import './js/modal';
+import './js/modalShow';
+import './js/modal-footer';
+import './js/foter-scrolltop';
+import './js/pagination';
+import './js/search';
+>>>>>>> Stashed changes
