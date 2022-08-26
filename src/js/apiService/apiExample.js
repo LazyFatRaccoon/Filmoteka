a6e9180c1a8570f953d29ebfae5b4e057b9fa58a
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-26 09:03:44 +0300

    modif api

--- src/js/apiService/apiExample.js
+++ src/js/apiService/apiExample.js
@@ -6,6 +6,9 @@ const refs = {
     movieByIdBtn: document.querySelector("#movie_id_request"), 
     genresBtn: document.querySelector("#genres_request"), 
     trailersBtn: document.querySelector("#trailers_request"), 
+    modifiedPopularBtn: document.querySelector("#modified_popular_request"),
+    modifiedSearchBtn: document.querySelector("#modified_search_request"),
+    modifiedSingleMoviehBtn: document.querySelector("#modified_single_movie_request"),
 }
 
 refs.popularBtn.addEventListener('click', consolePopular)
@@ -13,8 +16,13 @@ refs.searchBtn.addEventListener('click', consoleSearch)
 refs.movieByIdBtn.addEventListener('click', consoleMovieById)
 refs.genresBtn.addEventListener('click', consoleGenres)
 refs.trailersBtn.addEventListener('click', consoleTrailers)
+refs.modifiedPopularBtn.addEventListener('click', consoleModifiedPopular)
+refs.modifiedSearchBtn.addEventListener('click', consoleModifiedSearch)
+refs.modifiedSingleMoviehBtn.addEventListener('click', consoleModifiedSingleMovie)
 
-
+const watchedList = JSON.parse(localStorage.getItem('wachedList')) || [];
+watchedList.push(756999);
+localStorage.setItem('watchedList', JSON.stringify(watchedList));
 
 async function consolePopular() {
     const data = await API.getPopularMovies(1)
@@ -38,5 +46,20 @@ async function consoleGenres() {
 
 async function consoleTrailers() {
     const data = await API.getMovieTrailers(756999)
+    console.log(data)
+}
+
+async function consoleModifiedPopular() {
+    const data = await API.getModifiedMoviesList(1)
+    console.log(data)
+}
+
+async function consoleModifiedSearch() {
+    const data = await API.getModifiedMoviesList(1, 'cat')
+    console.log(data)
+}
+
+async function consoleModifiedSingleMovie() {
+    const data = await API.getModifiedSingleMovie(756999)
     console.log(data)
 }
