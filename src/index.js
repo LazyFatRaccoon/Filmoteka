1147ce2c43900303e0688ea01cb1c21814260859
OleksandrHorzhij <horzhij@gmail.com>
2022-08-26 17:05:07 +0300

    add js for button in modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './js/apiService/apiExample';
 import './js/createGallery';
-import './js/modal';
+// import './js/modal';
 import './js/modalShow';
 import './js/modal-footer';
 import './js/foter-scrolltop';
 import './js/pagination';
+import './js/modalBtn'
