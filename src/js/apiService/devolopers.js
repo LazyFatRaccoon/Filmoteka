7dffba264ef7cf2530a02b6b1ef937c9abc4825d
Kravtsov <kravtsovofficial99@gmail.com>
2022-08-27 23:09:46 +0300

    Доробив закриття модалки

--- src/js/apiService/devolopers.js
+++ src/js/apiService/devolopers.js
@@ -1,12 +1,12 @@
 
 export const devolopers = [
-  { name: "Raccoon", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
-  { name: "Станіслав Тищенко", image: "./images/Stanislav-Tishchenko.webp", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
-  { name: "Олександо Горжій", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"" },
-  { name: "Андрій Бачинский", image: "./images/Andriy-Bachynskyi.webp", work: "Tim-Lid", media:"https://github.com/Bachynckyi" },
-  { name: "Євгеній Медяник", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"" },
-  { name: "Олексадр Кравцов", image: "./images/Oleksandr-Kravtsov.webp", work: "Tim-Lid", media:"https://github.com/Kravtsov9983" },
-  { name: "Олександр Толочко", image: "./images/Oleksandr-Tolochko.webp", work: "Tim-Lid", media:"https://github.com/Alex-T-T" },
-  { name: "Володимир Болгов", image: "./images/Volodymyr-Bolgov.webp", work: "Tim-Lid", media:"https://github.com/VladymyrBolgov" },
-  { name: "Станіслав Завадський", image:  "./images/Stanislav-Zavadskyi.webp", work:"Tim-Lid", media:"https://github.com/uncle-Stas" },
+  { name: "Raccoon", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Raccoon3.webp", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
+  { name: "Станіслав Тищенко", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Stanislav-Tishchenko.webp", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
+  { name: "Олександо Горжій", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Raccoon3.webp", work: "Tim-Lid", media:"" },
+  { name: "Андрій Бачинский", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Andriy-Bachynskyi.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Bachynckyi" },
+  { name: "Євгеній Медяник", image: "https://ca.slack-edge.com/T03FC3278EP-U03GGA6TRDF-efd5d59a9f30-512", work: "Tim-Lid", media:"https://github.com/roowdy" },
+  { name: "Олексадр Кравцов", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Oleksandr-Kravtsov.webp", work: "Tim-Lid", media:"https://github.com/Kravtsov9983" },
+  { name: "Олександр Толочко", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Oleksandr-Tolochko.webp", work: "Tim-Lid", media:"https://github.com/Alex-T-T" },
+  { name: "Володимир Болгов", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/e8a03abefe52588d157bdf32342956382bcf127e/src/images/Volodymyr-Bolgov.jpg", work: "Tim-Lid", media:"https://github.com/VladymyrBolgov" },
+  { name: "Станіслав Завадський", image:  "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Stanislav-Zavadskyi.webp", work:"Tim-Lid", media:"https://github.com/uncle-Stas" },
 ] ;
--- src/js/apiService/modal-footer.js
+++ src/js/apiService/modal-footer.js
@@ -5,16 +5,17 @@ const refs = {
     closeModalBtn: document.querySelector(".modal-close-btn"),
     Background: document.querySelector(".backdrop-footer"),
     modal: document.querySelector(".modal-footer"),
-    list: document.querySelector(".list-developer")
-  };
+    list: document.querySelector(".list-developer"),
+    body: document.querySelector("body")
+};
 
-  // refs.openModalBtn.addEventListener("click", toggleModal);
-  // refs.closeModalBtn.addEventListener("click", toggleModal);
-  // refs.Background.addEventListener("click", escapeBackground);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.Background.addEventListener("click", escapeBackground);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-footer");
-    refs.Background.classList.toggle("is-hidden-footer");
+    refs.modal.classList.toggle("open-footer");
+    refs.Background.classList.toggle("open-footer");
     return
 }
 
@@ -22,75 +23,42 @@ window.addEventListener("keydown", keypressCounter)
 
 function keypressCounter(event) {
   const { code } = event;
-  if (code === "Escape" &&! refs.modal.classList.contains("is-hidden-footer")) {
-    refs.modal.classList.toggle("is-hidden-footer");
-    refs.Background.classList.toggle("is-hidden-footer");
+  if (code === "Escape" && refs.modal.classList.contains("open-footer")) {
+    refs.modal.classList.toggle("open-footer");
+    refs.Background.classList.toggle("open-footer");
     return
  }
 }
 
 function escapeBackground() {
-  if (!refs.modal.classList.contains("is-hidden-footer")) {
-    refs.modal.classList.toggle("is-hidden-footer");
-    refs.Background.classList.toggle("is-hidden-footer");
+  if (refs.modal.classList.contains("open-footer")) {
+    refs.modal.classList.toggle("open-footer");
+    refs.Background.classList.toggle("open-footer");
     return
   }
 }
 
-// const devolopers = [
-//   { name: "Raccoon", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
-//   { name: "Станіслав Тищенко", image: "./images/Stanislav-Tishchenko.webp", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
-//   { name: "Олександо Горжій", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"" },
-//   { name: "Андрій Бачинский", image: "./images/Andriy-Bachynskyi.webp", work: "Tim-Lid", media:"https://github.com/Bachynckyi" },
-//   { name: "Євгеній Медяник", image: "./images/Raccoon3.webp", work: "Tim-Lid", media:"" },
-//   { name: "Олексадр Кравцов", image: "./images/Oleksandr-Kravtsov.webp", work: "Tim-Lid", media:"https://github.com/Kravtsov9983" },
-//   { name: "Олександр Толочко", image: "./images/Oleksandr-Tolochko.webp", work: "Tim-Lid", media:"https://github.com/Alex-T-T" },
-//   { name: "Володимир Болгов", image: "./images/Volodymyr-Bolgov.webp", work: "Tim-Lid", media:"https://github.com/VladymyrBolgov" },
-//   { name: "Станіслав Завадський", image:  "./images/Stanislav-Zavadskyi.webp", work:"Tim-Lid", media:"https://github.com/uncle-Stas" },
-// ] ;
 
 function createDiv(array) {
-    return array.reduce((acc, item) => acc + `<li>
-<img src="${item.image}" alt="${item.name}" >
+    return array.reduce((acc, item) => acc + `<div class="developer-list">
+<img class="developer-foto" src="${item.image}" alt="${item.name}" >
+
+<div class="developer-diskription" >
 <div class="box-item">
     <p class="developer-name">${item.name}</p>
     <p class="developer-work">${item.work}</p>
-</div>
-  <li class="box-list">
-    <a class="footer-link" href=${item.media} target="_blank">
-    <svg class="svg" width="30" height="30">
-    <use href="./images/symbol-defs.svg#icon-github-icon"></use>
-    </svg>
-    </a>
+    
   </li>
-</li>`, "")
+</div>
+<div class="git-hub">
+<li class="box-list">
+    <a class="footer-link" href=${item.media} target="_blank">
+    <img class="footer-logo" src="https://icones.pro/wp-content/uploads/2021/06/icone-github-orange.png" alt="GIT HUB" >
+    </a>
+</div>
+</div>
+
+</div>`, "")
 }
 
-
-refs.list.insertAdjacentHTML("beforeend", createDiv(devolopers) )
-
-// devolopers.forEach(function (name, image, work, media) {
-  
-// })
-
-// const createListDeveloper = (devolopers) => `<li>
-// <img rsc="${devolopers.image}" alt="${devolopers.name}">
-// <div class="box-item">
-//     <p class="developer-name">${devolopers.name}</p>
-//     <p class="developer-work">${devolopers.work}</p>
-// </div>
-//   <li class="box-list">
-//     <a class="footer-link" href=${devolopers.media} target="_blank">
-//     <svg class="svg" width="30" height="30">
-//     <use href="./images/symbol-defs.svg#icon-github-icon"></use>
-//     </svg>
-//     </a>
-//   </li>
-// </li>`;
-
-// const generatorContent = (array) => array.raduce((acc, item) => acc + createListDeveloper(item), "");
-
-// const insertContent = (array) => {
-//   const result = generatorContent(array);
-//   list.insertAdjacentHTML("beforeend", result)
-// }
+refs.list.insertAdjacentHTML("beforeend", createDiv(devolopers));
