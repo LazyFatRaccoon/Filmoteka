c392a06f3255aff7e6bc0dfae991d2f50bf7e05f
Kravtsov <kravtsovofficial99@gmail.com>
2022-08-28 15:02:15 +0300

    Update devolopers.js

--- src/js/apiService/devolopers.js
+++ src/js/apiService/devolopers.js
@@ -1,12 +1,12 @@
 
 export const devolopers = [
-  { name: "Володимиор Тодоров", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Raccoon3.webp", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
-  { name: "Станіслав Тищенко", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Stanislav-Tishchenko.webp", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
-  { name: "Олександо Горжій", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Raccoon3.webp", work: "Tim-Lid", media:"https://github.com/AlexHorzhij" },
+  { name: "Володимиор Тодоров", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Volodymyr-Todorov.png?raw=true", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
+  { name: "Станіслав Тищенко", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Stanislav-Tishchenko.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
+  { name: "Олександо Горжій", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Horzhiy-Oleksandr.JPG?raw=true", work: "Tim-Lid", media:"https://github.com/AlexHorzhij" },
   { name: "Андрій Бачинский", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Andriy-Bachynskyi.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Bachynckyi" },
-  { name: "Євгеній Медяник", image: "https://ca.slack-edge.com/T03FC3278EP-U03GGA6TRDF-efd5d59a9f30-512", work: "Tim-Lid", media:"https://github.com/roowdy" },
-  { name: "Олексадр Кравцов", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Oleksandr-Kravtsov.webp", work: "Tim-Lid", media:"https://github.com/Kravtsov9983" },
-  { name: "Олександр Толочко", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Oleksandr-Tolochko.webp", work: "Tim-Lid", media:"https://github.com/Alex-T-T" },
-  { name: "Володимир Болгов", image: "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/e8a03abefe52588d157bdf32342956382bcf127e/src/images/Volodymyr-Bolgov.jpg", work: "Tim-Lid", media:"https://github.com/VladymyrBolgov" },
-  { name: "Станіслав Завадський", image:  "https://raw.githubusercontent.com/LazyFatRaccoon/Filmoteka/main/src/images/Stanislav-Zavadskyi.webp", work:"Tim-Lid", media:"https://github.com/uncle-Stas" },
+  { name: "Євгеній Медяник", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Evgeny-Medyanik.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/roowdy" },
+  { name: "Олексадр Кравцов", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Oleksandr-Kravtsov.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Kravtsov9983" },
+  { name: "Олександр Толочко", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Oleksandr-Tolochko.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Alex-T-T" },
+  { name: "Володимир Болгов", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Volodymyr-Bolgov.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/VladymyrBolgov" },
+  { name: "Станіслав Завадський", image:  "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Stanislav-Zavadskyi.jpg?raw=true", work:"Tim-Lid", media:"https://github.com/uncle-Stas" },
 ] ;
