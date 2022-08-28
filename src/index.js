b61e1cf6d19b33ad40571059f8600982b253e03e
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-28 15:01:22 +0300

    footerModalFix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import './js/apiService/apiExample';
+import './js/devolopers';
 
 import './js/firebase/firebaseAuth';
 import './js/firebase/dataBase';
 
 import './js/createGallery';
-// import './js/modal';
+//import './js/modal';
+
 import './js/modalShow';
-// import './js/modal-footer';
+import './js/modal-footer';
 import './js/foter-scrolltop';
 import './js/pagination';
 import './js/modalBtn';
