d99f5c3ef503b9865775484cdf60b02a669f5185
Kravtsov <kravtsovofficial99@gmail.com>
2022-08-28 20:18:35 +0300

    Правки

--- src/js/devolopers.js
+++ src/js/devolopers.js
@@ -1,7 +1,7 @@
 export const devolopers = [
   { name: "Володимир Тодоров", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Volodymyr-Todorov.png?raw=true", work: "Team Leader", media:"https://github.com/LazyFatRaccoon" },
   { name: "Станіслав Тищенко", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Stanislav-Tishchenko.jpg?raw=true", work: "Developer", media:"https://github.com/Stanislav2022" },
-  { name: "Олександо Горжій", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Horzhiy-Oleksandr.JPG?raw=true", work: "Developer", media:"https://github.com/AlexHorzhij" },
+  { name: "Олександр Горжій", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Horzhiy-Oleksandr.JPG?raw=true", work: "Developer", media:"https://github.com/AlexHorzhij" },
   { name: "Андрій Бачинский", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Andriy-Bachynskyi.jpg?raw=true", work: "Developer", media:"https://github.com/Bachynckyi" },
   { name: "Євгеній Медяник", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Evgeny-Medyanik.jpg?raw=true", work: "Developer", media:"https://github.com/roowdy" },
   { name: "Олексадр Кравцов", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Oleksandr-Kravtsov.jpg?raw=true", work: "Developer", media:"https://github.com/Kravtsov9983" },
