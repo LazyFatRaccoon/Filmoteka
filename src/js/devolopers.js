76137d8623bd71891a9d5b631f0729b5849a049d
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-28 16:08:04 +0300

    devFix

--- src/js/devolopers.js
+++ src/js/devolopers.js
@@ -1,5 +1,5 @@
 export const devolopers = [
-  { name: "Володимиор Тодоров", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Volodymyr-Todorov.png?raw=true", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
+  { name: "Володимир Тодоров", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Volodymyr-Todorov.png?raw=true", work: "Tim-Lid", media:"https://github.com/LazyFatRaccoon" },
   { name: "Станіслав Тищенко", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Stanislav-Tishchenko.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Stanislav2022" },
   { name: "Олександо Горжій", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Horzhiy-Oleksandr.JPG?raw=true", work: "Tim-Lid", media:"https://github.com/AlexHorzhij" },
   { name: "Андрій Бачинский", image: "https://github.com/LazyFatRaccoon/Filmoteka/blob/main/src/images/Andriy-Bachynskyi.jpg?raw=true", work: "Tim-Lid", media:"https://github.com/Bachynckyi" },
