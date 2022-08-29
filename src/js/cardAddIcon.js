b6c623f07c547bc29971e248ac7dd8a85d1e6f48
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-29 18:57:08 +0300

    addIconFix

--- src/js/cardAddIcon.js
+++ src/js/cardAddIcon.js
@@ -9,20 +9,26 @@ export function cardAddIcon() {
 
     const card = document.querySelector(`[data-id="${id}"]`)
     console.log('id', id)
-    // console.log(card)
+    console.log('card', card)
     const svgIcon = card.querySelector("svg")
     const useIcon = card.querySelector("use")
 
+    console.log('svgIcon', svgIcon)
+    console.log('useIcon', useIcon)
+
     const queueIcon = "/Filmoteka/login-icon.2e0a9156.svg#icon-queue"
     const watchedIcon ="/Filmoteka/login-icon.2e0a9156.svg#icon-watched"
+
     console.log(watched, queue)
     if (watched) {
-        svgIcon.style.opacity = "1" 
-        useIcon.href=`${watchedIcon}`        
+        svgIcon.style.opacity = "1"; 
+        useIcon.href='';
+        useIcon.href=`${watchedIcon}`;        
     }
     if (queue) {
-        svgIcon.style.opacity = "1" 
-        useIcon.href=`${queueIcon}`        
+        svgIcon.style.opacity = "1"; 
+        useIcon.href='';
+        useIcon.href=`${queueIcon}`;        
     }
     if ((!queue) && (!watched)) {
         svgIcon.style.opacity = "0"        
