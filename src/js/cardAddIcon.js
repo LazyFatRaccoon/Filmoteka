9cf89386abbd6da8de7b31096dad5ed53fba2488
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-08-29 18:48:04 +0300

    fix

--- src/js/cardAddIcon.js
+++ src/js/cardAddIcon.js
@@ -1,20 +1,21 @@
 export function cardAddIcon() {
     const watchedList = document.querySelector('.add-btn__watched')
     const queueList = document.querySelector('.add-btn__queue')
-    console.log(watchedList)
+    console.log('watchedList ', watchedList)
+    console.log("queueList ", queueList)
     const id = watchedList.id;
     const watched = watchedList.classList.contains('disabl')
     const queue = queueList.classList.contains('disabl')
 
     const card = document.querySelector(`[data-id="${id}"]`)
-    console.log(id)
-    console.log(card)
+    console.log('id', id)
+    // console.log(card)
     const svgIcon = card.querySelector("svg")
     const useIcon = card.querySelector("use")
 
     const queueIcon = "/Filmoteka/login-icon.2e0a9156.svg#icon-queue"
     const watchedIcon ="/Filmoteka/login-icon.2e0a9156.svg#icon-watched"
-
+    console.log(watched, queue)
     if (watched) {
         svgIcon.style.opacity = "1" 
         useIcon.href=`${watchedIcon}`        
