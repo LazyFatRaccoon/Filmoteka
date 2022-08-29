5eea870b320192e845da87750775aaf0f68c585b
roowdy <medyanik.ua@gmail.com>
2022-08-29 20:45:32 +0300

    add-themes

--- src/js/themization.js
+++ src/js/themization.js
@@ -9,16 +9,28 @@ if (currentTheme == "dark") {
 }
 
 // Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function(e) {
   // Переключаем класс .dark-theme при каждом щелчке
   document.body.classList.toggle("dark-theme");
   // Допустим, тема светлая
+ 
+
   let theme = "light";
   // Если <body> содержит класс .dark-theme…
   if (document.body.classList.contains("dark-theme")) {
     // …тогда делаем тему тёмной
+  
     theme = "dark";
   }
   // После чего сохраняем выбор в localStorage
   localStorage.setItem("theme", theme);
+  localStorage.setItem("btnToggle", btnToggle);
+
 });
+
+
+document.querySelector(".btn-toggle").addEventListener("click", function(e) {
+  
+  e.target.closest(".btn-toggle").classList.toggle("btn-toggle--toggle");
+
+})
